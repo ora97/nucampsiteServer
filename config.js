ed06dca34331d25f97023b9b69ad31777c98ee80
ope <oranimashaun@gmail.com>
2021-10-23 18:56:02 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook' : {
+        clientId: '1530513510624541',
+        clientSecret: '71092756dd1735f5cd9d37008b8d61cb'
+    }
 }
